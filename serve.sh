1be9230da8db1227a5639fb7485d6b5d1d063e98
Emil Woźniak <emil.wozniak.591986@gmail.com>
2022-12-04 14:26:03 +0100

    add serve

--- serve.sh
+++ serve.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-cargo install --git https://github.com/DioxusLabs/cli
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+cargo install --verbose --git https://github.com/DioxusLabs/cli
 dioxus serve --platform web
